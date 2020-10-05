86fff22c22fab302d54651f671ea4690ef60ef34
Vijet M <vijet.mahabaleshwar@okta.com>
2020-10-05 15:15:38 -0700

    Adds required env vars to e2e script (#3)
    * Adds required env vars to e2e script
    
    * prebuild before e2e
    
    Co-authored-by: Aaron Granick <aaron.granick@okta.com>

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,8 +2,17 @@
 
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
+setup_service google-chrome-stable 83.0.4103.61-1
+
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/test-reports/e2e"
+
+export ISSUER=https://samples-javascript.okta.com/oauth2/default
+export SPA_CLIENT_ID=0oapmwm72082GXal14x6
+export WEB_CLIENT_ID=0oapmx9r5dK1dDAd54x6
+export USERNAME=george@acme.com
+get_secret prod/okta-sdk-vars/client_secret CLIENT_SECRET
+get_secret prod/okta-sdk-vars/password PASSWORD
 
 if ! yarn test:e2e; then
   echo "unit failed! Exiting..."
