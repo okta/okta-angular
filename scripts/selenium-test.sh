e56b4e71a849ccc84f5ba979072eef230b021006
Kostya <kostiantyn.drozdov@okta.com>
2020-12-02 16:07:54 +0200

    Fixed script

--- scripts/selenium-test.sh
+++ scripts/selenium-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 setup_service xvfb
-setup_service google-chrome-stable 86.0.4240.22
+setup_service google-chrome-stable 83.0.4103.61-1
 
 ls -a
-cd test/selenium-test/
+cd /root/okta/okta-angular
 ls -a
 docker ps -a
 docker-compose up -d
@@ -12,7 +12,7 @@ sleep 300
 docker ps
 docker logs --tail 40 angular_backend
 npm install selenium-webdriver
-wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
