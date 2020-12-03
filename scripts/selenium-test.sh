1f9135d3a537d2904e94e152ac3a3593a041c9c3
Kostya <kostiantyn.drozdov@okta.com>
2020-12-03 14:41:06 +0200

    Added echo for commands

--- scripts/selenium-test.sh
+++ scripts/selenium-test.sh
@@ -13,14 +13,26 @@ docker ps
 docker logs --tail 40 angular_backend
 cd /root/okta/okta-angular/test/selenium-test/selenium
 pwd
+echo npm install selenium-webdriver
 npm install selenium-webdriver
-echo here
 ls -a
 pwd
 wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
+
+echo unzip chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+
+echo mv chromedriver /usr/bin/chromedriver
 mv chromedriver /usr/bin/chromedriver
+
+echo chown root:root /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
+
+echo chmod +x /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
+
+echo pwd
 pwd
+
+
 node /root/okta/okta-angular/test/selenium-test/selenium/okta-angular-widget-test.ts
