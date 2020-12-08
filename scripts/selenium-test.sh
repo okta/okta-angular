18fdcd754b5c2036de92819233b60edf6da0c5e9
Kostya <kostiantyn.drozdov@okta.com>
2020-12-08 16:45:26 +0200

    Removed extra echo commands

--- scripts/selenium-test.sh
+++ scripts/selenium-test.sh
@@ -2,40 +2,19 @@
 setup_service xvfb
 setup_service google-chrome-stable 83.0.4103.61-1
 
-ls -a
 cd /root/okta/okta-angular
-ls -a
-docker ps -a
+
 docker-compose up -d
-docker ps
-sleep 420
-echo check port
-netstat -tulnp
+sleep 300
+
 docker ps
 docker logs --tail 40 angular_backend
 cd /root/okta/okta-angular/test/selenium-test/selenium
-pwd
-echo npm install selenium-webdriver
 npm install selenium-webdriver
-ls -a
-pwd
 wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
-
-echo unzip chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-
-echo mv chromedriver /usr/bin/chromedriver
 mv chromedriver /usr/bin/chromedriver
-
-echo chown root:root /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
-
-echo chmod +x /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
-
-echo pwd
-pwd
-
-
 node /root/okta/okta-angular/test/selenium-test/selenium/okta-angular-widget-test.ts
 curl http://localhost:9000/
