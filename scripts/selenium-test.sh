e48977e346ad4e21d0201963b48e024031eae45d
Kostya <kostiantyn.drozdov@okta.com>
2021-01-08 13:48:27 +0200

    Updated chrome and chromedriver version, refactored code.

--- scripts/selenium-test.sh
+++ scripts/selenium-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setup_service xvfb
-setup_service google-chrome-stable 83.0.4103.61-1
+setup_service google-chrome-stable-85.0.4183.102-1
 
 cd /root/okta/okta-angular
 
@@ -14,7 +14,7 @@ sleep 120
 
 cd /root/okta/okta-angular/test/selenium-test/selenium
 npm install selenium-webdriver
-wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/87.0.4280.88/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
--- test/selenium-test/selenium/okta-angular-widget-test.ts
+++ test/selenium-test/selenium/okta-angular-widget-test.ts
@@ -1,7 +1,6 @@
 var webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 var chromeCapabilities = webdriver.Capabilities.chrome();
-
 var browser = new webdriver.Builder().forBrowser('chrome')
                                          .setChromeOptions(new chrome.Options().addArguments(
                                               "--headless",
@@ -16,39 +15,26 @@ var browser = new webdriver.Builder().forBrowser('chrome')
                                          ))
                                          .build();
 var assert = require('assert');
-(async function example() {
+(async function e2eTest() {
     try {
-
         browser.manage().setTimeouts( { implicit: 10000 } );
         browser.get('http://localhost:9000/');
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.xpath('//button[@routerlink=\'/login\']')), 10000).click();
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.name('username')), 10000).sendKeys(`${process.env.SIW_TEST_USER_EMAIL}`);
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.name('password')), 10000).sendKeys(`${process.env.SIW_TEST_USER_PASSWORD}`);
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.id('okta-signin-submit')), 10000).click();
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.xpath('//button[@routerlink=\'/protected\']')), 10000).click();
-
         var protectedEndPoint = await browser.wait(webdriver.until.elementLocated(webdriver.By.xpath('//app-secure[text()="Protected endpoint!"]')), 10000).getText();
-
         console.log(protectedEndPoint);
         assert.deepStrictEqual(protectedEndPoint,'Protected endpoint!');
-
         await browser.wait(webdriver.until.elementLocated(webdriver.By.xpath('//*[contains(text(),"Home")]')), 10000).click();
         await browser.wait(webdriver.until.elementLocated(webdriver.By.xpath('//*[contains(text(),"Logout")]')), 10000).click();
-
         await browser.quit();
         console.log("Test passed");
-
     }
     catch(err){
         await browser.quit();
         await console.error(err);
         process.exit(1);
     }
-    
-
 })();
