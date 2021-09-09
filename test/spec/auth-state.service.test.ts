81dfd7a13dd83288e609ac8b8022c6a9cb085d03
Shuo Wu <wushuo2010@gmail.com>
2021-09-09 11:54:55 -0400

    unit test

--- test/spec/auth-state.service.test.ts
+++ test/spec/auth-state.service.test.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Observable } from 'rxjs';
-import { AuthState, OktaAuth } from '@okta/okta-auth-js';
+import { AuthState, OktaAuth, UserClaims } from '@okta/okta-auth-js';
 import { 
   OktaAuthModule, 
   OktaAuthStateService, 
@@ -83,6 +83,247 @@ describe('OktaAuthStateService', () => {
       expect(fn).toHaveBeenNthCalledWith(1, { isAuthenticated: false });
       expect(fn).toHaveBeenNthCalledWith(2, states[0]);
       expect(fn).toHaveBeenNthCalledWith(3, states[1]);
+    });
+  });
+
+  describe('hasAnyGroups', () => {
+    describe('isAuthenticated === false', () => {
+      it('observes false result', () => {
+        jest.spyOn(oktaAuth.authStateManager, 'getAuthState').mockReturnValue({ isAuthenticated: false } as AuthState);
+        setup(oktaAuth);
+        const service: OktaAuthStateService = TestBed.inject(OktaAuthStateService);
+        return new Promise(resolve => {
+          service.hasAnyGroups(['mock']).subscribe(result => {
+            expect(result).toEqual(false);
+            resolve(null);
+          });
+        });
+      });
+    });
+
+    describe('idToken not exist', () => {
+      it('observes false result', () => {
+        jest.spyOn(oktaAuth.authStateManager, 'getAuthState').mockReturnValue({ idToken: undefined } as AuthState);
+        setup(oktaAuth);
+        const service: OktaAuthStateService = TestBed.inject(OktaAuthStateService);
+        return new Promise(resolve => {
+          service.hasAnyGroups(['mock']).subscribe(result => {
+            expect(result).toEqual(false);
+            resolve(null);
+          });
+        });
+      });
+    });
+
+    describe('isAuthenticated === true and idToken exist', () => {
+      describe('has groups claim', () => {
+        let service: OktaAuthStateService;
+        beforeEach(() => {
+          jest.spyOn(oktaAuth.authStateManager, 'getAuthState')
+            .mockReturnValue({ 
+              isAuthenticated: true,
+              idToken: { 
+                claims: {
+                  groups: ['test']
+                } 
+              }
+            } as unknown as AuthState);
+          setup(oktaAuth);
+          service = TestBed.inject(OktaAuthStateService);
+        });
+
+        describe('can verify with string input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups('test').subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups('non-exist-group').subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+        describe('can verify with array input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups(['test']).subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups(['non-exist-group']).subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+        describe('can verify with object input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ groups: ['test'] }).subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ groups: ['non-exist-group'] }).subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+      });
+
+      describe('has custom claims', () => {
+        let service: OktaAuthStateService;
+        beforeEach(() => {
+          jest.spyOn(oktaAuth.authStateManager, 'getAuthState')
+            .mockReturnValue({ 
+              isAuthenticated: true,
+              idToken: { 
+                claims: {
+                  'custom-groups': ['test']
+                } 
+              }
+            } as unknown as AuthState);
+          jest.spyOn(oktaAuth, 'getUser').mockResolvedValue({
+            'custom-groups': ['test']
+          } as unknown as UserClaims);
+          setup(oktaAuth);
+          service = TestBed.inject(OktaAuthStateService);
+        });
+        describe('can verify with object input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ 'custom-groups': ['test'] }).subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ 'custom-groups': ['non-exist-group'] }).subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+        it('fails with array input', () => {
+          return new Promise(resolve => {
+            service.hasAnyGroups(['test']).subscribe(result => {
+              expect(result).toEqual(false);
+              resolve(null);
+            });
+          });
+        });
+        it('fails with string input', () => {
+          return new Promise(resolve => {
+            service.hasAnyGroups('test').subscribe(result => {
+              expect(result).toEqual(false);
+              resolve(null);
+            });
+          });
+        });
+      });
+
+      describe('has thin idToken (groups claim not in idToken)', () => {
+        let service: OktaAuthStateService;
+        beforeEach(() => {
+          jest.spyOn(oktaAuth.authStateManager, 'getAuthState')
+            .mockReturnValue({ 
+              isAuthenticated: true,
+              idToken: {
+                claims: {}
+              }
+            } as unknown as AuthState);
+          jest.spyOn(oktaAuth, 'getUser').mockResolvedValue({
+            groups: ['test']
+          } as unknown as UserClaims);
+          setup(oktaAuth);
+          service = TestBed.inject(OktaAuthStateService);
+        });
+
+        it('calls oktaAuth.getUser()', () => {
+          return new Promise(resolve => {
+            service.hasAnyGroups('test').subscribe(() => {
+              expect(oktaAuth.getUser).toHaveBeenCalled();
+              resolve(null);
+            });
+          });
+        });
+
+        describe('can verify with string input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups('test').subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups('non-exist-group').subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+        describe('can verify with array input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups(['test']).subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups(['non-exist-group']).subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+        describe('can verify with object input', () => {
+          it('observes true when groups match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ groups: ['test'] }).subscribe(result => {
+                expect(result).toEqual(true);
+                resolve(null);
+              });
+            });
+          });
+          it('observes false when groups not match', () => {
+            return new Promise(resolve => {
+              service.hasAnyGroups({ groups: ['non-exist-group'] }).subscribe(result => {
+                expect(result).toEqual(false);
+                resolve(null);
+              });
+            });
+          });
+        });
+      });
+
     });
   });
 });
