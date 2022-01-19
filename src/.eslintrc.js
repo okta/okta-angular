d9a9e45ad75e1c1da84991ced50c83fed89ef794
Jared Perreault <jared.perreault@okta.com>
2022-01-19 11:16:04 -0500

    adds eslint import/no-extraneous-dependencies rule

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -5,6 +5,27 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   plugins: [
-    '@typescript-eslint'
-  ]
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': false
+    }]
+  },
+  settings: {
+    // https://github.com/import-js/eslint-plugin-import#typescript
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    },
+    // https://github.com/import-js/eslint-plugin-import#importcore-modules
+    'import/core-modules': [
+      '@angular/core',
+      '@angular/common',
+      '@angular/router',
+      'rxjs',
+      'rxjs/operators'
+    ]
+  },
 };
