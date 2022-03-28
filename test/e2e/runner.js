ef45a9e9ba7891c14d084550b0ecff7509cbd395
denys.oblohin <denys.oblohin@okta.om>
2022-03-28 23:33:27 +0300

    test from 13 to 7: should fail on 11
    .
    leave 12 13

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -47,11 +47,6 @@ const getTask = (app) => () => {
 // track process returnCode for each task
 const codes = [];
 const tasks = [
-  'angular-v7',
-  'angular-v8',
-  'angular-v9',
-  'angular-v10',
-  'angular-v11',
   'angular-v12',
   'angular-v13',
   'angular-v14',
