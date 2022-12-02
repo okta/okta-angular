d7fe451ae46cb3c928ff549b8b8932eeeb88034d
denys.oblohin <denys.oblohin@okta.com>
2022-12-02 15:44:03 +0200

    remove unused config

--- test/apps/angular-v14/jest.config.js
+++ test/apps/angular-v14/jest.config.js
@@ -2,11 +2,7 @@ export default {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
-  transform: {
-    //'^.+\\.(ts|js)$': 'jest-preset-angular'
-  },
   transformIgnorePatterns: [
-    //'node_modules/(?!@angular|@okta|rxjs|jsonpath-plus)'
   ],
   testMatch: [
     '**/*.spec.{js,ts}'
