13a68b7d281d38af737b008cafecf8f833f84c0f
denys.oblohin <denys.oblohin@okta.com>
2022-12-09 12:34:16 +0200

    yarn test:apps:prepare .

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -88,15 +88,12 @@ if ! yarn build; then
   exit ${FAILED_SETUP}
 fi
 
-pushd ./dist
-  npx yalc publish
-popd
 
 # Install dependencies for test apps
+yarn test:apps:prepare
 for app in test/apps/angular-*
 do
   pushd $app
-    npx yalc add @okta/okta-angular
     yarn install
     install_auth_js
   popd
