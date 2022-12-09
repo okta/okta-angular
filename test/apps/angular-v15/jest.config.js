6a009a95083bbd0dbdc73a8f5d011fbbec3452d5
denys.oblohin <denys.oblohin@okta.com>
2022-12-09 19:37:06 +0200

    enable jest test for angular-15 .

---
+++ test/apps/angular-v15/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$|rxjs|@okta/okta-auth-js|jsonpath-plus|@okta/okta-angular)'
+  ],
+  testMatch: [
+    '**/*.spec.{js,ts}'
+  ],
+};
