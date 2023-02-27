a763157a6b00ce2eede151e910da362871b317c1
Jared Perreault <jared.perreault@okta.com>
2023-02-27 11:50:18 -0500

    first pass

--- lib/src/okta/has-any-group.directive.ts
+++ lib/src/okta/has-any-group.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { OktaAuthStateService, Groups } from './services/auth-state.service';
 
 @Directive({ selector: '[oktaHasAnyGroup]'})
-export class OktaHasAnyGroupDirective {
+export class OktaHasAnyGroupDirective implements OnChanges, OnDestroy {
   private previousIsAuthorized: boolean;
+  private hasAnyGroups$: Subscription;
+  private groups: Groups;
 
   constructor(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,8 +15,22 @@ export class OktaHasAnyGroupDirective {
     private authStateService: OktaAuthStateService
   ) { }
 
-  @Input() set oktaHasAnyGroup(groups: Groups) {
-    this.authStateService.hasAnyGroups(groups)
+  ngOnDestroy(): void {
+    this.hasAnyGroups$.unsubscribe();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.groups.currentValue !== changes.groups.previousValue) {
+      this.subscribeToAnyGroups();
+    }
+  }
+
+  subscribeToAnyGroups() {
+    if (this.hasAnyGroups$) {
+      this.hasAnyGroups$.unsubscribe();
+    }
+
+    this.hasAnyGroups$ = this.authStateService.hasAnyGroups(this.groups)
       .subscribe(isAuthorized => {
         // don't update UI if no state change
         if (isAuthorized === this.previousIsAuthorized) {
@@ -25,5 +42,21 @@ export class OktaHasAnyGroupDirective {
           this.viewContainer.createEmbeddedView(this.templateRef);
         }
       });
+  }
+
+  @Input() set oktaHasAnyGroup(groups: Groups) {
+    this.groups = groups;
+    // this.hasAnyGroups$ = this.authStateService.hasAnyGroups(groups)
+    //   .subscribe(isAuthorized => {
+    //     // don't update UI if no state change
+    //     if (isAuthorized === this.previousIsAuthorized) {
+    //       return;
+    //     }
+    //     this.previousIsAuthorized = isAuthorized;
+    //     this.viewContainer.clear();
+    //     if (isAuthorized) {
+    //       this.viewContainer.createEmbeddedView(this.templateRef);
+    //     }
+    //   });
   }
 }
