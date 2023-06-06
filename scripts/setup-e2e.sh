eea59544a206cac5ed12041b63cac8ceeae9f2f8
eng-prod-CI-bot-okta <shaw.yu@okta.com>
2023-06-06 00:46:10 +0000

    Downstream Artifact Test for commit 4c72de84d3216c5d964986c4d83382301f7dda2b in artifact okta-auth-js

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -3,7 +3,7 @@
 # Can be used to run a canary build against a beta AuthJS version that has been published to artifactory.
 # This is available from the "downstream artifact" menu on any okta-auth-js build in Bacon.
 # DO NOT MERGE ANY CHANGES TO THIS LINE!!
-export AUTHJS_VERSION=""
+export AUTHJS_VERSION="7.4.0-g4c72de8"
 
 # Install a specific version of auth-js, used by downstream artifact builds
 install_auth_js () {
