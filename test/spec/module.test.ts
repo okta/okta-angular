eee04a1f69be12a6577d797d8cc3adf92c148673
denys.oblohin <denys.oblohin@okta.com>
2023-11-27 18:53:14 +0200

    lint fix

--- test/spec/module.test.ts
+++ test/spec/module.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, APP_INITIALIZER } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
